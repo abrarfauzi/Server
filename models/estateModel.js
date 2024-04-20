3c535701209f6df9d29df9278f517007b63a8775
abrar <abrar.fauzi@e.braude.ac.il>
2024-04-20 18:10:13 +0300

    first commit

---
+++ models/estateModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const estateSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    saleOrRent: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 2.5,
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Estate = mongoose.model("Estate", estateSchema);
+
+export default Estate;
